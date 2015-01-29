1bbc16bcc22b2970a58e65759143ed4a82179b22
Simon <simon@hoetten.org>
2015-01-29 20:35:48 +0100

    trying to get cycle detection working & updating data.

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -1,10 +1,12 @@
 import csv
 import networkx as nx
+import operator
 
 
-G = nx.DiGraph()
+G = nx.DiGraph()							# new directed graph
 
-# read graph from csv
+######## read graph from csv
+############################
 with open('data/graph.csv', 'rb') as csvfile:
 	reader = csv.reader(csvfile, delimiter=';', quotechar='"')
 	for rowIdx, row in enumerate(reader):
@@ -17,8 +19,29 @@ with open('data/graph.csv', 'rb') as csvfile:
 						if weight != 0:
 							G.add_edge(rowIdx, colIdx, weight=weight)
 
-# analyze graph
-print G.nodes()
+
+############## analyze graph
+############################
+print "Nodes:",len(G)
+print "Edges:",len(G.edges())
 # print G.edges()
-print list(nx.strongly_connected_components(G))
-# print len(list(nx.simple_cycles(G)))
+
+### centrality
+degrees = nx.degree_centrality(G)
+sorted_degrees = sorted(degrees.items(), key=operator.itemgetter(1), reverse=True)		# sort for degree
+names = nx.get_node_attributes(G,'name')
+
+for node in sorted_degrees:
+	print G.degree(node[0]), node[0],":",names[node[0]]
+
+absDegrees = {k: round(v*len(G)-1) for k, v in degrees.items()}
+# print absDegrees
+
+
+##### cycle (loops) analysis
+############################
+copyG = G.copy()
+copyG.remove_node(6)		# remove pflanzenanbau
+
+print "SCCs:", list(nx.strongly_connected_components(copyG))
+print "Cycles:",len(list(nx.simple_cycles(copyG)))
